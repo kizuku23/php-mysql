55b98f5cd0ac8725b31b2372dfd077fd09b74a07
KIZUKU <km.dwc23@gmail.com>
2020-12-08 15:40:05 +0900

    [Finish] 4-59

---
+++ memo/index.php
@@ -0,0 +1,40 @@
+<!doctype html>
+<html lang="ja">
+<head>
+<!-- Required meta tags -->
+<meta charset="utf-8">
+<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+<!-- Bootstrap CSS -->
+<link rel="stylesheet" href="../css/style.css">
+
+<title>PHP</title>
+</head>
+<body>
+<header>
+<h1 class="font-weight-normal">PHP</h1>    
+</header>
+
+<main>
+<h2>Practice</h2>
+<pre>
+<?php
+try {
+  $db = new PDO('mysql:dbname=mydb;host=localhost;charaset=utf8','root','root');
+} catch(PDOException $e) {
+  echo 'DB接続エラー: ' .$e->getMessage();
+}
+
+// $count = $db->exec('INSERT INTO my_items SET maker_id=1, item_name="もも", price=210, keyword="缶詰，ピンク，甘い"');
+// echo $count .'件のデータを挿入しました';
+
+$records = $db->query('SELECT * FROM my_items');
+while ($record = $records->fetch()) {
+  print($record['item_name'] ."\n");
+}
+
+?>
+</pre>
+</main>
+</body>    
+</html>
