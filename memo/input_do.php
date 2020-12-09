b04cc264ff963effaa1e84680a91cbbfcaa48b7a
KIZUKU <km.dwc23@gmail.com>
2020-12-09 21:06:23 +0900

    [Finish] 4-62

--- memo/input_do.php
+++ memo/input_do.php
@@ -21,8 +21,13 @@
   <?php
   try {
     $db = new PDO('mysql:dbname=mydb;host=localhost;charset=utf8', 'root', 'root');
-    $db->exec('INSERT INTO memos SET memo="' .$_POST['memo'] .'", created_at=NOW()');
 
+    $statement = $db->prepare('INSERT INTO memos SET memo=?, created_at=NOW()');
+    // $db->exec('INSERT INTO memos SET memo="' .$_POST['memo'] .'", created_at=NOW()');
+    $statement->bindParam(1, $_POST['memo']);
+    $statement->execute();
+    // $statement->execute(array($_POST['memo']));
+    echo 'メッセージが登録されました';
   } catch(PDOException $e) {
     echo 'DB接続エラー: ' .$e->getMessage();
   }
